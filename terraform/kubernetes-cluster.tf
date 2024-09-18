fb5acc56492cefc53c70feb22f39f1cd2ddbc82d
anhkhoado932 <139329468+anhkhoado932@users.noreply.github.com>
2024-09-18 21:55:30 +1000

    ok i get it

--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -30,5 +30,4 @@ resource "azurerm_role_assignment" "role_assignment" {
   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.container_registry.id
-  skip_service_principal_aad_check = true
 }

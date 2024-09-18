ea304a0eaaa9eaa5608d54fe6e42fb655bf9a1ce
anhkhoado932 <139329468+anhkhoado932@users.noreply.github.com>
2024-09-18 21:45:54 +1000

    add fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,13 +4,26 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71.0"
+      version = "~> 4.x"
     }
   }
 
   required_version = ">= 1.5.6"
 }
+data "azurerm_client_config" "current" {}
 
+output "current_client_id" {
+  value = data.azurerm_client_config.current.client_id
+  sensitive = true
+}
+
+output "current_tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+}
+
+output "current_subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
 provider "azurerm" {
   features {}
 }
